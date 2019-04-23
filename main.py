389805aa6be2e38fe94dbc5a80f26d5c40461972
svsaraf@yahoo.com <svsaraf@yahoo.com>
2019-04-23 16:31:57 -0700

    bland helloworld-flask

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
   return 'Hey its Python Flask application deployed by Shree!'
 
 @app.route('/about')
