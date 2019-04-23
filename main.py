dcd518e8b16bd9a32e4114ba0cea8549c6b3988f
svsaraf@yahoo.com <svsaraf@yahoo.com>
2019-04-23 16:25:51 -0700

    bland helloworld-flask

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-
- posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
 @app.route("/")
-def home():
-    return 'Hey its Home or default route in  Python Flask application!'
+def hello_world():
+  return 'Hey its Python Flask application deployed by Shree!'
 
 @app.route('/about')
 def about():
