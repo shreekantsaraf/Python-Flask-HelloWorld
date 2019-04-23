eb7079ca49e507ddb85b125145bd6196de1d5532
svsaraf@yahoo.com <svsaraf@yahoo.com>
2019-04-23 16:40:25 -0700

    modified two routes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-def home():
-  return 'Hey its Python Flask application deployed by Shree - home route!'
+@app.route('/')
+@app.route('/home/')
+def index():
+    return 'Index Page'
 
-@app.route("/about")
-def about():
-  return 'Hey its Python Flask application -- about route!'
+@app.route('/hello/')
+def hello():
+    return 'Hello, World'
 	
 if __name__ == '__main__':
   app.run()
