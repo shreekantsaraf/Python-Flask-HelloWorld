0137ea5326984b0d783ed57db5362f6b949db8d4
svsaraf@yahoo.com <svsaraf@yahoo.com>
2019-04-23 15:50:34 -0700

    two routes

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+@app.route('/about')
+def about():
+  return 'Hey its about route in Python Flask application!'
 
+ posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return 'Hey its Home or default route in  Python Flask application!'
+  
 if __name__ == '__main__':
   app.run()
