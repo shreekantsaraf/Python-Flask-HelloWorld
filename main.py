4303f234c72dd886f6c3763f2dcb9ee9103d4dcd
svsaraf@yahoo.com <svsaraf@yahoo.com>
2019-04-23 16:34:46 -0700

    two routes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-  return 'Hey its Python Flask application deployed by Shree!'
+  return 'Hey its Python Flask application deployed by Shree - home route!'
 
-@app.route('/about')
+@app.route("/about")
 def about():
-  return 'Hey its about route in Python Flask application!'
+  return 'Hey its Python Flask application -- about route!'
 	
 if __name__ == '__main__':
   app.run()
